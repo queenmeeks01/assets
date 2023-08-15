ee900fba0350f6ccd323c0d7f8afab730b1290a2
queenmeeks01 <118959962+queenmeeks01@users.noreply.github.com>
2023-08-14 18:59:47 -0700

    Update values.go

--- internal/config/values.go
+++ internal/config/values.go
@@ -3,12 +3,12 @@ package config
 import "github.com/trustwallet/go-primitives/coin"
 
 var StackingChains = []coin.Coin{
-	coin.Tezos(),
+	coin.Tezos(5000),
 	coin.Cosmos(),
 	coin.Iotex(),
 	coin.Tron(),
 	coin.Waves(),
 	coin.Kava(),
 	coin.Terra(),
-	coin.Binance(),
+	coin.Binance(5000),
 }
